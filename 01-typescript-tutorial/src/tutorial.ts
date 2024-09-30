65dfb1907d7aa03e6ac1caab128ed5077ae4a6b6
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-30 15:11:15 +0200

    09. First Challenge
    -u tutorial.ts fajlu kreirane tri varijable

--- 01-typescript-tutorial/src/tutorial.ts
+++ 01-typescript-tutorial/src/tutorial.ts
@@ -1,13 +1,12 @@
-let awesomeName = 'shakeAndBake';
-awesomeName = 'something';
-// awesomeName = 20;
-awesomeName = awesomeName.toUpperCase();
-console.log(awesomeName);
+let greeting: string = 'Hello, TypeScript!';
+greeting = greeting.toUpperCase(); 
 
-let amount = 12;
-amount = 12 - 1;
-// amount = 'pants';
+let age: number = 25;
+age = age + 5; 
 
-let isAwesome = true;
-isAwesome = false;
-// isAwesome = 'shakeAndBake';
+let isAdult: boolean = age >= 18;
+isAdult = !isAdult; 
+
+// greeting = 10;
+// age = 'thirty';
+// isAdult = 'yes'; 
