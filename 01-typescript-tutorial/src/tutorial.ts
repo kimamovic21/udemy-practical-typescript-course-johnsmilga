cc7974fd99033f815b654190468faf1d7edbe3c9
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-30 15:02:27 +0200

    07. Type Annotations
    -u tutorial.ts fajlu kreirane tri varijable
    -varijablama dodan tip

--- 01-typescript-tutorial/src/tutorial.ts
+++ 01-typescript-tutorial/src/tutorial.ts
@@ -1,13 +1,13 @@
-console.log('typescript tutorial');
+let awesomeName: string = 'shakeAndBake';
+awesomeName = 'something';
+// awesomeName = 20;
+awesomeName = awesomeName.toUpperCase();
+console.log(awesomeName);
 
-interface someValue {
-    name: string;
-    id: number;
-};
+let amount: number = 12;
+amount = 12 - 1;
+// amount = 'pants';
 
-let someObj: someValue = {
-    name: 'Kerim',
-    id: 123
-};
-
-console.log(someObj);
+let isAwesome: boolean = true;
+isAwesome = false;
+// isAwesome = 'shakeAndBake';
