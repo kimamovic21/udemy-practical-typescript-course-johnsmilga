5cf8bac92e6e86fd743ea805ebf73ecffb6b2e56
kimamovic21 <imamovic.kerim@gmail.com>
2024-09-30 14:45:25 +0200

    06. Build Step
    -u tutorial.ts fajlu kreiran interface someValue i varijabla someObj

--- 01-typescript-tutorial/src/tutorial.ts
+++ 01-typescript-tutorial/src/tutorial.ts
@@ -1,1 +1,13 @@
 console.log('typescript tutorial');
+
+interface someValue {
+    name: string;
+    id: number;
+};
+
+let someObj: someValue = {
+    name: 'Kerim',
+    id: 123
+};
+
+console.log(someObj);
