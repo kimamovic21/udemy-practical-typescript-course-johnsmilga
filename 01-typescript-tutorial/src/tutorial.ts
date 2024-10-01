1c44d7315b7ee599c348462bafcb7218269cd16d
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-01 14:12:21 +0200

    24. Functions - Default Parameters
    -u tutorial.ts fajlu kreirana funkcija sa tipom i dva parametra sa tipovima u kojoj jedan parametar ima default vrijednost

--- 01-typescript-tutorial/src/tutorial.ts
+++ 01-typescript-tutorial/src/tutorial.ts
@@ -1,6 +1,19 @@
-function calculatePrice(price: number, discount?: number):number {
+function calculatePrice(price: number, discount?: number): number {
   return price - (discount || 0);
 };
 
 let priceAfterDiscount = calculatePrice(100, 20);
 console.log(priceAfterDiscount); 
+
+let priceWithoutDiscount = calculatePrice(300);
+console.log(priceWithoutDiscount); 
+
+function calculateScore(initialScore: number, penaltyPoints: number = 0): number {
+  return initialScore - penaltyPoints;
+};
+
+let scoreAfterPenalty = calculateScore(100, 20);
+console.log(scoreAfterPenalty); 
+
+let scoreWithoutPenalty = calculateScore(300);
+console.log(scoreWithoutPenalty); 
