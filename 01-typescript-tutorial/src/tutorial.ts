d91b5324ff6e4da9778392dd94571f64fd5a503d
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-01 16:25:56 +0200

    30. Challenge - Functions II
    -u tutorial.ts fajlu kreirana funkcija sa parametrima
    -prvi parametar ima vrijednost string | number
    -drugi parametar je objekt parametar koji ima default vrijednost

--- 01-typescript-tutorial/src/tutorial.ts
+++ 01-typescript-tutorial/src/tutorial.ts
@@ -1,12 +1,16 @@
-function createStudent(student: { id: number; name: string }) {
-  console.log(`Welcome to the course ${student.name.toUpperCase()}!!!`);
+function processData(
+  input: string | number,
+  config: { reverse: boolean } = { reverse: false }
+): string | number {
+  if (typeof input === 'number') {
+    return input * input;
+  } else {
+    return config.reverse
+      ? input.toUpperCase().split('').reverse().join('')
+      : input.toUpperCase();
+  };
 };
 
-const newStudent = {
-  id: 5,
-  name: 'anna',
-  email: 'anna@gmail.com',
-};
-
-createStudent(newStudent);
-createStudent({ id: 1, name: 'bob', email: 'bob@gmail.com' });
+console.log(processData(10));
+console.log(processData('Hello')); 
+console.log(processData('Hello', { reverse: true })); 
