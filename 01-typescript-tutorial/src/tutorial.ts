9154f2420179217ddd9238c81450ef9b5f53508c
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-01 11:25:30 +0200

    19. Functions - Parameters
    -u tutorial.ts fajlu kreirana funkcija sa tipom

--- 01-typescript-tutorial/src/tutorial.ts
+++ 01-typescript-tutorial/src/tutorial.ts
@@ -1,14 +1,7 @@
-let bike: { brand: string; year: number } = { brand: 'Yamaha', year: 2010 };
-// bike.year = 'old';
-console.log(bike);
+function sayHi(name: string) {
+  console.log(`Hello there ${name.toUpperCase()}!!!`);
+};
 
-let laptop: { brand: string; year: number } = { brand: 'Dell', year: 2020 };
-// let laptop2: { brand: string, year: number } = { brand: 'HP' };
-console.log(laptop); 
-
-let product1 = { title: 'Shirt', price: 20 };
-let product2 = { title: 'Pants' };
-let products: { title: string; price?: number }[] = [product1, product2];
-
-// products.push({ title: 'Shoes', price: 'expensive' }); 
-console.log(products);
+sayHi('kerim');
+// sayHi(3);
+// sayHi('peter', 'random');
