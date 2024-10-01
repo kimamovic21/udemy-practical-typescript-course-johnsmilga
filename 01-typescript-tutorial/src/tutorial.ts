f758cf3827681f94a848544d3592cb44c26942bf
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-01 17:41:02 +0200

    36. Interface - Fundamentals
    -u tutorial.ts fajlu kreiran interfejs interface Book
    -varijabli const deepWork dodan tip Book

--- 01-typescript-tutorial/src/tutorial.ts
+++ 01-typescript-tutorial/src/tutorial.ts
@@ -1,8 +1,18 @@
-const propName = 'age';
-
-type Animal = {
-  [propName]: number;
+interface Book {
+  readonly isbn: number;
+  title: string;
+  author: string;
+  genre?: string;
 };
 
-let tiger: Animal = { [propName]: 5 };
-console.log(tiger);
+const deepWork: Book = {
+  isbn: 9781455586691,
+  title: 'Deep Work',
+  author: 'Cal Newport',
+  genre: 'Self-help',
+};
+
+// deepWork.title = 'New Title'; // allowed
+// deepWork.isbn = 654321; // not allowed
+
+console.log(deepWork);
