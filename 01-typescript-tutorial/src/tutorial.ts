27b015f3a48406c1fc2f8f34cf0f821f5f12826c
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-02 14:11:26 +0200

    39. Challenge - Interface I
    -kreiran interfejs Computer
    -kreirana varijabla const laptop koja ima tipComputer

--- 01-typescript-tutorial/src/tutorial.ts
+++ 01-typescript-tutorial/src/tutorial.ts
@@ -1,48 +1,22 @@
-interface Book {
-  readonly isbn: number;
-  title: string;
-  author: string;
-  genre?: string;
-   // method
-  printAuthor(): void;
-  printTitle(message: string): string;
-  printSomething: (someValue: number) => number;
+interface Computer {
+  readonly id: number;
+  brand: string;
+  ram: number;
+  upgradeRam(increase: number): number;
+  storage?: number;
 };
 
-const deepWork: Book = {
-  isbn: 9781455586691,
-  title: 'Deep Work',
-  author: 'Cal Newport',
-  genre: 'Self-help',
-  // printAuthor() {
-  //   console.log(this.author);
-  // },
-  printTitle(message) {
-    return `${this.title} ${message}`;
-  },
-  // first option
-  // printSomething: function (someValue) {
-  //   return someValue;
-  // },
-  // second option
-  // printSomething: (someValue) => {
-  //   console.log(deepWork.author);
-  //   return someValue;
-  // },
-  // third option
-  printSomething(someValue) {
-    return someValue;
-  },
-  // alternative
-  printAuthor: () => {
-    console.log(deepWork.author);
+const laptop: Computer = {
+  id: 1,
+  brand: 'random brand',
+  ram: 8, 
+  upgradeRam(amount: number) {
+    this.ram += amount;
+    return this.ram;
   },
 };
 
-console.log(deepWork);
-deepWork.printAuthor();
-const result = deepWork.printTitle('is an awesome book');
-console.log(result);
+laptop.storage = 256; 
 
-console.log(deepWork.printSomething(34));
-deepWork.printAuthor();
+console.log(laptop.upgradeRam(4)); 
+console.log(laptop);
