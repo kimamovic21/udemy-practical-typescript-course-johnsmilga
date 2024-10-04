52a7ba7fa9e12e60cb440cb6432a4d7b7e8e2bbe
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-04 15:21:13 +0200

    65. Generics - Multiple Types
    -u tutorial.ts fajlu kreirana funkcija pair<T, U>() sa prametrima param1 i param2
    -prvom parametru param1 dodan tip T
    -drugom parametru param2 dodan tip U
    -funkciji pair() dodan tip [T, U]
    -kreirana varijabla let result

--- 01-typescript-tutorial/src/tutorial.ts
+++ 01-typescript-tutorial/src/tutorial.ts
@@ -1,19 +1,6 @@
-function generateStringArray(length: number, value: string): string[] {
-  let result: string[] = [];
-  result = Array(length).fill(value);
-  return result;
+function pair<T, U>(param1: T, param2: U): [T, U] {
+  return [param1, param2];
 };
 
-console.log(generateStringArray(3, 'hello'));
-
-function createArray<T>(length: number, value: T): Array<T> {
-  let result: T[] = [];
-  result = Array(length).fill(value);
-  return result;
-};
-
-let arrayStrings = createArray<string>(3, 'hello');
-let arrayNumbers = createArray<number>(4, 100);
-
-console.log(arrayStrings);
-console.log(arrayNumbers);
+let result = pair<number, string>(123, 'Hello');
+console.log(result);
