05ee23ff55e56ee5c638373c1218c78b0964bd7f
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-04 16:43:39 +0200

    69. Fetch Data - Basics
    -kreirana varijabla const url
    -kreirana asinhrona funkcija fetchData() sa parametrom url koji ima tip string
    -u funkciji fetchData() kreirani blokovi koda try - catch
    -u bloku koda try kreirana varijabla const response
    -kreirana varijabla const tours

--- 01-typescript-tutorial/src/tutorial.ts
+++ 01-typescript-tutorial/src/tutorial.ts
@@ -1,13 +1,25 @@
-interface StoreData<T = any> {
-  data: T[];
+const url = 'https://www.course-api.com/react-tours-project';
+
+async function fetchData(url: string) {
+  try {
+    const response = await fetch(url);
+
+    // Check if the request was successful.
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    };
+
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    const errMsg = error instanceof Error ? error.message : 'there was an error...';
+    console.error(errMsg);
+    return [];
+  };
 };
 
-const storeNumbers: StoreData<number> = {
-  data: [1, 2, 3, 4],
-};
-console.log(storeNumbers);
-
-const randomStuff: StoreData = {
-  data: ['random', 1],
-};
-console.log(randomStuff);
+const tours = await fetchData(url);
+tours.map((tour: any) => {
+  console.log(tour.name);
+});
