c22d36076ab6984562a46bdc24ad33f9078d3d80
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-05 14:20:56 +0200

    73. Declaration Files
    -kreiran types.ts fajl i unutar njega tip Random
    -u tutorial.ts fajl importovan tip Random

--- 01-typescript-tutorial/README.md
+++ 01-typescript-tutorial/README.md
@@ -2181,7 +2181,10 @@ tours.map((tour) => {
 
 ## Typescript Declaration File
 
-In TypeScript, .d.ts files, also known as declaration files,consist of type definitions, and are used to provide types for JavaScript code. They allow TypeScript to understand the shape and types of objects, functions, classes, etc., in JavaScript libraries, enabling type checking and autocompletion in TypeScript code that uses these libraries.
+In TypeScript, .d.ts files, also known as declaration files,consist of type definitions, and are used to 
+provide types for JavaScript code. They allow TypeScript to understand the shape and types of objects, 
+functions, classes, etc., in JavaScript libraries, enabling type checking and autocompletion in TypeScript
+code that uses these libraries.
 
 - create types.ts
 - export RandomType
--- 01-typescript-tutorial/src/tutorial.ts
+++ 01-typescript-tutorial/src/tutorial.ts
@@ -1,43 +1,6 @@
-import { z } from 'zod';
+import { Random } from "./types";
 
-const url = 'https://www.course-api.com/react-tours-project';
-
-const tourSchema = z.object({
-  id: z.string(),
-  name: z.string(),
-  info: z.string(),
-  image: z.string(),
-  price: z.string(),
-  // something: z.string(),
-});
-
-type Tour = z.infer<typeof tourSchema>;
-
-async function fetchData(url: string): Promise<Tour[]> {
-  try {
-    const response = await fetch(url);
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    };
-
-    const rawData: Tour[] = await response.json();
-    const result = tourSchema.array().safeParse(rawData);
-    
-    if (!result.success) {
-      throw new Error(`Invalid data: ${result.error}`);
-    };
-
-    return result.data;
-  } catch (error) {
-    const errMsg = error instanceof Error ? error.message : 'there was an error...';
-    console.error(errMsg);
-    return [];
-  };
+function myName(name: Random): void {
+  console.log(`My name is ${name}.`)
 };
-
-const tours = await fetchData(url);
-tours.map((tour) => {
-  console.log(tour.name);
-//   console.log(tour.something);
-});
+myName('Kerim');
