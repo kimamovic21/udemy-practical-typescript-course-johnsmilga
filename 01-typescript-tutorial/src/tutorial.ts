556f69d3900e0cf0967446d306214bcb5d5492a4
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-05 12:58:02 +0200

    72. Zod Library
    -u terminalu instaliran zod paket
    -u tutorial.ts fajlu dodana instanca z iz paketa zod
    -kreirana varijabla const tourSchema za validaciju varijabli

--- 01-typescript-tutorial/src/tutorial.ts
+++ 01-typescript-tutorial/src/tutorial.ts
@@ -1,26 +1,34 @@
+import { z } from 'zod';
+
 const url = 'https://www.course-api.com/react-tours-project';
 
-type Tour = {
-  id: string;
-  name: string;  
-  info: string;
-  image: string;
-  price: string;
-//   something: string;
-};
+const tourSchema = z.object({
+  id: z.string(),
+  name: z.string(),
+  info: z.string(),
+  image: z.string(),
+  price: z.string(),
+  // something: z.string(),
+});
+
+type Tour = z.infer<typeof tourSchema>;
 
 async function fetchData(url: string): Promise<Tour[]> {
   try {
     const response = await fetch(url);
 
-    // Check if the request was successful.
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     };
 
-    const data: Tour[] = await response.json();
-    console.log(data);
-    return data;
+    const rawData: Tour[] = await response.json();
+    const result = tourSchema.array().safeParse(rawData);
+    
+    if (!result.success) {
+      throw new Error(`Invalid data: ${result.error}`);
+    };
+
+    return result.data;
   } catch (error) {
     const errMsg = error instanceof Error ? error.message : 'there was an error...';
     console.error(errMsg);
