45a9a6461fdc13477393058b76b5d42c574df7f9
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-05 14:44:08 +0200

    78. Classes - Public and Private Modifiers
    -u tutorial.ts fajlu, u klasi Book varijablama title i author dodana opcija public
    -varijabli checkedOut dodana opcija private
    -kreirane public metode checkOut() i isCheckedOut()
    -kreirana private metoda toggleCheckedOutStatus()

--- 01-typescript-tutorial/src/tutorial.ts
+++ 01-typescript-tutorial/src/tutorial.ts
@@ -1,15 +1,24 @@
 class Book {
-  readonly title: string;
-  author: string;
-  checkedOut: boolean = false;
+  public readonly title: string;
+  public author: string;
+  private checkedOut: boolean = false;
   constructor(title: string, author: string) {
     this.title = title;
     this.author = author;
   };
+  public checkOut() {
+    this.checkedOut = this.toggleCheckedOutStatus();
+  };
+  public isCheckedOut() {
+    return this.checkedOut;
+  };
+  private toggleCheckedOutStatus() {
+    return !this.checkedOut;
+  };
 };
 
-const deepWork = new Book('deep work ', 'cal newport');
-deepWork.checkedOut = true;
-// deepWork.checkedOut = 'something else';
-deepWork.title = 'something else';
+const deepWork = new Book('Deep Work', 'Cal Newport');
 console.log(deepWork);
+deepWork.checkOut();
+console.log(deepWork.isCheckedOut());
+// deepWork.toggleCheckedOutStatus(); 
