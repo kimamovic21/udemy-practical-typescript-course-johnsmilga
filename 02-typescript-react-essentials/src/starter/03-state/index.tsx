caa36bb09301f93935e3bb6287d5d9a479e133ef
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-07 11:22:40 +0200

    98. State - Custom List
    -u folderu 03-state, u index.tsx fajl, kreirana varijabla const navLinks
    -kreiran tip Link
    -varijabli navLinks dodan tip Link[]
    -kreirana state varijabla links i set funkcija setLinks
    -u dogadaj onClick proslijedena funkcija setLinks()
    -funkciji useState() dodan tip <Link[]>

--- 02-typescript-react-essentials/src/starter/03-state/index.tsx
+++ 02-typescript-react-essentials/src/starter/03-state/index.tsx
@@ -1,9 +1,34 @@
 import { useState } from 'react'
 
+type Link = {
+  id: number
+  url: string
+  text: string
+}
+
+const navLinks: Link[] = [
+  {
+    id: 1,
+    url: 'https://reactjs.org',
+    text: 'react docs',
+  },
+  {
+    id: 2,
+    url: 'https://reactrouter.com',
+    text: 'react router docs',
+  },
+  {
+    id: 3,
+    url: 'https://reacttraining.com',
+    text: 'react training',
+  },
+]
+
 function Component() {
   const [text, setText] = useState('shakeAndBake')
   const [number, setNumber] = useState(1)
   const [list, setList] = useState<string[]>([])
+  const [links, setLinks] = useState<Link[]>(navLinks)
 
   return (
     <div>
@@ -11,7 +36,8 @@ function Component() {
       <button className='btn btn-center' onClick={() => {
         // setText('shakeAndBake')
         // setNumber(23)
-        setList(['john', 'peter'])
+        // setList(['john', 'peter'])
+        setLinks([...links, { id: 4, url: 'hello', text: 'hello' }])
       }}>
         click me
       </button>
