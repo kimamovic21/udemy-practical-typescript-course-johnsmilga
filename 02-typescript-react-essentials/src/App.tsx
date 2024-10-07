7298d5f1bf2e824a72da142b3cd2823464b356ac
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-07 11:02:52 +0200

    96. Children Prop
    -u folder 02-props, u index.tsx fajlu, u tip ComponentProps dodan prop children? koji ima tip React.ReactNode
    -dodan prop tip PropsWithChildren
    -u App.tsx komponenti dodana komponenta Component sa otvorenim i zatvorenim tagom

--- 02-typescript-react-essentials/src/App.tsx
+++ 02-typescript-react-essentials/src/App.tsx
@@ -3,7 +3,9 @@ import Component from './starter/02-props'
 function App() {
   return (
     <main>
-      <Component name='john' id={1} />
+      <Component name='john' id={1} >
+        <h2>Hello World</h2>
+      </Component>
       <Component name='peter' id={2} />
     </main>
   )
--- 02-typescript-react-essentials/src/starter/02-props/index.tsx
+++ 02-typescript-react-essentials/src/starter/02-props/index.tsx
@@ -1,13 +1,16 @@
-type ComponentProps = {
+import { type PropsWithChildren } from 'react'
+
+type ComponentProps = PropsWithChildren<{
   name: string
   id: number
-}
+}>
 
-function Component({ name, id }: ComponentProps) {
+function Component({ name, id, children }: ComponentProps) {
   return (
     <div>
       <h2>Name: {name}</h2>
       <h2>Id: {id}</h2>
+      {children}
     </div>
   )
 }
