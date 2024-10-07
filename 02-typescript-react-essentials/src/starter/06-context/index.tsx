32cdb2d7be415c0ccf3cce7223ba3b319bec1944
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-07 16:07:23 +0200

    105. Context API - Basic Example
    -u folderu 06-context kreiran context.tsx fajl
    -u komponentu App.tsx dodana komponenta Component iz foldera 06-context
    -dodana logika za Context API

--- 02-typescript-react-essentials/src/starter/06-context/index.tsx
+++ 02-typescript-react-essentials/src/starter/06-context/index.tsx
@@ -1,9 +1,22 @@
+import { useTheme, ThemeProvider } from './context'
+
+function ParentComponent() {
+  return (
+    <ThemeProvider>
+      <Component />
+    </ThemeProvider>
+  )
+}
+
 function Component() {
+  const context = useTheme()
+  console.log(context)
+
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>Context API</h2>
+      <h2>random component</h2>
     </div>
-  );
+  )
 }
-export default Component;
+
+export default ParentComponent
