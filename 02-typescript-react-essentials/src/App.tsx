7c51b80b9dc244e86645d9cf341668162f6ca337
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-07 11:13:20 +0200

    97. State
    -u folderu 03-state, u index.tsx fajl, importovana i kreirana fukcija useState()
    -kreirane state varijable text, number i list
    -kreirane set funkcije setText, setNumber i setList
    -u jsx-u kreiran button element sa prop className i dogadajem onClick
    -u dogadaj onClick proslijedene set funkcije

--- 02-typescript-react-essentials/src/App.tsx
+++ 02-typescript-react-essentials/src/App.tsx
@@ -1,12 +1,9 @@
-import Component from './starter/02-props'
+import Component from './starter/03-state'
 
 function App() {
   return (
     <main>
-      <Component name='john' id={1} >
-        <h2>Hello World</h2>
-      </Component>
-      <Component name='peter' id={2} />
+      <Component />
     </main>
   )
 }
--- 02-typescript-react-essentials/src/starter/03-state/index.tsx
+++ 02-typescript-react-essentials/src/starter/03-state/index.tsx
@@ -1,9 +1,22 @@
+import { useState } from 'react'
+
 function Component() {
+  const [text, setText] = useState('shakeAndBake')
+  const [number, setNumber] = useState(1)
+  const [list, setList] = useState<string[]>([])
+
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>State</h2>
+      <h2 className='mb-1'>React & Typescript</h2>
+      <button className='btn btn-center' onClick={() => {
+        // setText('shakeAndBake')
+        // setNumber(23)
+        setList(['john', 'peter'])
+      }}>
+        click me
+      </button>
     </div>
-  );
+  )
 }
-export default Component;
+
+export default Component
