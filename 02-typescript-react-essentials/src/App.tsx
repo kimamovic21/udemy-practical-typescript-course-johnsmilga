6762c617745cfa90b2bb97d0d4509552cb793529
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-07 14:54:20 +0200

    103. Challenge - Alternative Solution
    -u komponenti App.tsx, komponenti Component dodan prop email

--- 02-typescript-react-essentials/src/App.tsx
+++ 02-typescript-react-essentials/src/App.tsx
@@ -3,8 +3,7 @@ import Component from './starter/05-challenge'
 function App() {
   return (
     <main>
-      <Component />
-      <Component type='basic' name='susan' />
+      <Component type='basic' name='susan' email='' />
       <Component type='advanced' name='anna' email='anna@email.com' />
     </main>
   )
