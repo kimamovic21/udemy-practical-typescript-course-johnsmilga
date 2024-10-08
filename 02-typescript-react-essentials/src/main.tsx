c516ae2a5eb47168eecee22e0c5aaa60c081d859
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-08 13:50:11 +0200

    112. Fetch Data - React Query and Axios
    -u terminalu instalirani paketi React Query and Axios
    -u main.jsx dodane komponente iz paketa Tanstack
    -u types.ts fajl dodana logika za paket Axios
    -u index.tsx fajl dodana logika za paket Tanstack

--- 02-typescript-react-essentials/src/main.tsx
+++ 02-typescript-react-essentials/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+const queryClient = new QueryClient();
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <QueryClientProvider client={queryClient}>
     <App />
-  </StrictMode>,
-)
+  </QueryClientProvider>
+);
