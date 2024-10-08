dd1b1ac12b5b43d937a254ac8aa149e1f1ef3549
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-08 11:44:12 +0200

    110. Fetch Data - UseEffect and Fetch API
    -u komponentu App.tsx dodana komponenta Component iz foldera 08-fetch-data
    -u folderu 08-fetch-data, u index.tsx fajl dodana logika za dohvacanje podataka o turama

--- 02-typescript-react-essentials/src/starter/08-fetch-data/index.tsx
+++ 02-typescript-react-essentials/src/starter/08-fetch-data/index.tsx
@@ -1,9 +1,53 @@
+import { useState, useEffect } from 'react'
+
+const url = 'https://www.course-api.com/react-tours-project'
+
 function Component() {
+  const [tours, setTours] = useState([])
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [isError, setIsError] = useState<string | null>(null)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true)
+      try {
+        const response = await fetch(url)
+        if (!response.ok) {
+          throw new Error(`Failed to fetch tours...`)
+        }
+        const rawData = await response.json()
+        setTours(rawData)
+        console.log(rawData)
+      } catch (error) {
+        const message =
+          error instanceof Error ? error.message : 'there was an error...'
+        setIsError(message)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    fetchData()
+  }, [])
+
+  if (isLoading) {
+    return <h3>Loading...</h3>
+  }
+
+  if (isError) {
+    return <h3>Error: {isError}</h3>
+  }
+
   return (
     <div>
       <h2>React & Typescript</h2>
+      {tours.map((tour) => {
+        return (
+          <p key={tour.id}>{tour.name}</p>
+        )
+      })}
       <h2>Fetch Data</h2>
     </div>
-  );
+  )
 }
-export default Component;
+
+export default Component
