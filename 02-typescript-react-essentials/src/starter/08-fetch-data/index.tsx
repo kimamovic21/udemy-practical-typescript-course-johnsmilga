c573c38aec070c294446ac1a22350bb7e9df5a06
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-08 11:57:04 +0200

    111. Fetch Data - Type and Schema
    -u folderu 08-fetch-data kreiran types.ts fajl
    -u types.ts fajlu kreirana varijabla const tourSchema i tip Tour
    -u terminalu instaliran zod paket
    -u index.tsx fajl dodana zod logika prilikom dohvacanja podataka

--- 02-typescript-react-essentials/src/starter/08-fetch-data/index.tsx
+++ 02-typescript-react-essentials/src/starter/08-fetch-data/index.tsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react'
+import { type Tour, tourSchema } from './types'
 
 const url = 'https://www.course-api.com/react-tours-project'
 
 function Component() {
-  const [tours, setTours] = useState([])
+  const [tours, setTours] = useState<Tour[]>([])
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [isError, setIsError] = useState<string | null>(null)
 
@@ -15,12 +16,18 @@ function Component() {
         if (!response.ok) {
           throw new Error(`Failed to fetch tours...`)
         }
-        const rawData = await response.json()
-        setTours(rawData)
-        console.log(rawData)
+
+        const rawData:Tour[] = await response.json()
+        const result = tourSchema.array().safeParse(rawData)
+
+        if (!result.success) {
+          console.log(result.error.message)
+          throw new Error(`Failed to parse tours`)
+        }
+
+        setTours(result.data)
       } catch (error) {
-        const message =
-          error instanceof Error ? error.message : 'there was an error...'
+        const message = error instanceof Error ? error.message : 'there was an error...'
         setIsError(message)
       } finally {
         setIsLoading(false)
@@ -39,13 +46,14 @@ function Component() {
 
   return (
     <div>
-      <h2>React & Typescript</h2>
+      <h2 className='mb-1'>Tours</h2>
       {tours.map((tour) => {
         return (
-          <p key={tour.id}>{tour.name}</p>
+          <p key={tour.id} className='mb-1'>
+            {tour.name}
+          </p>
         )
       })}
-      <h2>Fetch Data</h2>
     </div>
   )
 }
