2ba440c18baf79fbae372b0f72f25c0f47db134d
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-09 15:38:04 +0200

    133. Link Component
    -u komponentu Link.tsx dodane komponente Link i Button

--- 03-store-project/src/pages/Cart.tsx
+++ 03-store-project/src/pages/Cart.tsx
@@ -1,6 +1,17 @@
+import { Button } from '@/components/ui/button'
+import { Link } from 'react-router-dom'
+
 function Cart() {
   return (
-    <div>Cart</div>
+    <div>
+      <h2 className='text-4xl'>Cart Page</h2>
+      <Link to='/' className='text-2xl text-red-900'>
+        back home
+      </Link>
+      <Button asChild size='lg'>
+        <Link to='/'>home button</Link>
+      </Button>
+    </div>
   )
 }
 
