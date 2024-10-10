a89a8d4de26e1075b2f4d28a44b05e74642fa37e
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-10 11:46:05 +0200

    140. Links Dropdown Component
    -folder utils premjesten u folder src
    -u terminalu instaliran paket za komponentu Dropdown Menu
    -u folderu ui generisana komponenta dropdown-menu.tsx
    -u komponentu LinksDropdown.tsx dodane komponente iz dropdown-menu.tsx fajla

--- 03-store-project/src/components/LinksDropdown.tsx
+++ 03-store-project/src/components/LinksDropdown.tsx
@@ -1,6 +1,36 @@
+import { NavLink } from 'react-router-dom'
+import { AlignLeft } from 'lucide-react'
+import { links } from '@/utils'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { Button } from './ui/button'
+
 function LinksDropdown() {
   return (
-    <div>LinksDropdown</div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild className='lg:hidden'>
+        <Button variant='outline' size='icon'>
+          <AlignLeft />
+          <span className='sr-only'>Toggle links</span>
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className='w-52 lg:hidden' align='start' sideOffset={25}>
+        {links.map((link) => {
+          return (
+            <DropdownMenuItem key={link.label}>
+              <NavLink to={link.href} className={({ isActive }) => { return `capitalize w-full ${isActive ? 'text-primary' : ''}` }}>
+                {link.label}
+              </NavLink>
+            </DropdownMenuItem>
+          )
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
 
