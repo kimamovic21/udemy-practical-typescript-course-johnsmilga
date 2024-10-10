98e22b1c3258311d80caa02701b180bdc4b3580e
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-10 14:24:37 +0200

    141. NavLinks Component
    -u komponentu NavLinks.tsx dodana komponenta NavLink i niz links

--- 03-store-project/src/components/NavLinks.tsx
+++ 03-store-project/src/components/NavLinks.tsx
@@ -1,6 +1,25 @@
+import { NavLink } from 'react-router-dom'
+import { links } from '@/utils'
+
 function NavLinks() {
   return (
-    <div>NavLinks</div>
+    <div className='hidden lg:flex justify-center items-center gap-x-4'>
+      {links.map((link) => {
+        return (
+          <NavLink
+            to={link.href}
+            className={({ isActive }) => {
+              return `capitalize font-light tracking-wide ${
+                isActive ? 'text-primary' : ''
+              }`
+            }}
+            key={link.label}
+          >
+            {link.label}
+          </NavLink>
+        )
+      })}
+    </div>
   )
 }
 
