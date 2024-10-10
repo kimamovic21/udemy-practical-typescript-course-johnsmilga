6fdcd1239a97faca1ba1c5d478b8c93b09b815a5
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-10 17:06:47 +0200

    148. About Page Component
    -u komponenti About.tsx kreirani HTML elementi sa Tailwind CSS klasama

--- 03-store-project/src/pages/About.tsx
+++ 03-store-project/src/pages/About.tsx
@@ -1,6 +1,20 @@
 function About() {
   return (
-    <div>About</div>
+    <section>
+      <h2 className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center text-4xl font-bold leading-none tracking-tight sm:text-6xl '>
+        We love
+        <span className='bg-primary py-2 px-4 rounded-lg  tracking-widest text-white'>
+          comfy
+        </span>
+      </h2>
+
+      <p className='mt-6 text-lg tracking-wide leading-8 max-w-2xl mx-auto text-justify'>
+        Welcome to the world of ultimate comfort! Comfy, we are passionate about creating spaces where style meets relaxation. 
+        From plush sofas to ergonomic chairs, each piece of furniture is crafted with care and designed to make your home a haven 
+        of comfort and elegance. Whether you're curling up with a book or hosting a cozy gathering, our furniture adapts to your 
+        lifestyle, ensuring you feel at ease in every corner of your home.
+      </p>
+    </section>
   )
 }
 
