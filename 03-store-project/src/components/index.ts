a88b4a2b1f228bb71249d2e40ff652729e3813cf
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-12 14:55:45 +0200

    163. Global Loading - Setup
    -u folderu components kreirana komponenta Loading.tsx
    -u index.ts fajlu eksportovana komponenta Loading.tsx kao default
    -u komponentu HomeLayout.tsx dodana komponenta Loading.tsx i logika za prikazivanje komponente Loading kada se ocitavaju podaci o proizvodima na stranici

--- 03-store-project/src/components/index.ts
+++ 03-store-project/src/components/index.ts
@@ -14,4 +14,5 @@ export { default as ProductsGrid } from './ProductsGrid';
 export { default as Filters } from './Filters';
 export { default as ProductsContainer } from './ProductsContainer';
 export { default as PaginationContainer } from './PaginationContainer';
-export { default as ProductsList} from './ProductsList';
+export { default as ProductsList } from './ProductsList';
+export { default as Loading } from './Loading';
--- 03-store-project/src/pages/HomeLayout.tsx
+++ 03-store-project/src/pages/HomeLayout.tsx
@@ -1,15 +1,19 @@
-import { Header, Navbar } from '@/components'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigation } from 'react-router-dom'
+import { Header, Navbar, Loading } from '@/components'
 
-const HomeLayout = () => {
+function HomeLayout() {
+  const navigation = useNavigation()
+  const isPageLoading = navigation.state === 'loading'
+
   return (
     <>
       <Header />
       <Navbar />
+
       <div className='align-element py-20'>
-        <Outlet />
+        {isPageLoading ? <Loading /> : <Outlet />}
       </div>
-    </> 
+    </>
   )
 }
 
