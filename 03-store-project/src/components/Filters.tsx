175ad6170b56d4ccbc85d8a7e33cdc91312fd215
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-14 17:06:17 +0200

    169. Form Select
    -u folderu components kreirana komponenta FormSelect.tsx
    -u index.ts fajlu eksportovana komponenta FormSelect.tsx kao default
    -u komponentu Filters.tsx dodana komponenta FormSelect.tsx

--- 03-store-project/src/components/Filters.tsx
+++ 03-store-project/src/components/Filters.tsx
@@ -2,24 +2,51 @@ import { Form, Link, useLoaderData } from 'react-router-dom'
 import { ProductsResponseWithParams } from '@/utils'
 import { Button } from '@/components/ui/button'
 import FormInput from './FormInput'
+import FormSelect from './FormSelect'
 
 function Filters() {
   const { meta, params } = useLoaderData() as ProductsResponseWithParams
+  console.log(meta)
   const { search, company, category, shipping, order, price } = params
 
   return (
     <Form className='border rounded-md px-8 py-4 grid gap-x-4 gap-y-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
-      {/* search */}
+      {/* SEARCH */}
       <FormInput
         type='search'
         label='search product'
         name='search'
         defaultValue={search}
       />
+
+      {/* CATEGORIES */}
+      <FormSelect
+        label='select category'
+        name='category'
+        options={meta.categories}
+        defaultValue={category}
+      />
+
+      {/* COMPANIES */}
+      <FormSelect
+        label='select company'
+        name='company'
+        options={meta.companies}
+        defaultValue={company}
+      />
+
+      {/* ORDER */}
+      <FormSelect
+        label='order by'
+        name='order'
+        options={['a-z', 'z-a', 'high', 'low']}
+        defaultValue={order}
+      />
+
       <Button 
         type='submit' 
         size='sm' 
-        className='self-end mb-2'
+        className='self-end mb-2 capitalize'
       >
         search
       </Button>
@@ -29,7 +56,7 @@ function Filters() {
         asChild
         size='sm'
         variant='outline'
-        className='self-end mb-2'
+        className='self-end mb-2 capitalize'
       >
         <Link to='/products'>reset</Link>
       </Button>
--- 03-store-project/src/components/index.ts
+++ 03-store-project/src/components/index.ts
@@ -17,3 +17,4 @@ export { default as PaginationContainer } from './PaginationContainer';
 export { default as ProductsList } from './ProductsList';
 export { default as Loading } from './Loading';
 export { default as FormInput } from './FormInput';
+export { default as FormSelect } from './FormSelect';
