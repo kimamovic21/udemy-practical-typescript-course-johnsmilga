a02fe1de30ae4bf4892a991ee0b5e921a05f8af1
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-17 13:58:16 +0200

    183. Cart Slice - Setup
    -u types.ts fajlu kreirani i eksportovani tipovi CartItem i CartState
    -u cartSlice.ts fajlu kreirana varijabla const defaultState i funkcije u objektu reducers

--- 03-store-project/src/features/cart/cartSlice.ts
+++ 03-store-project/src/features/cart/cartSlice.ts
@@ -1,13 +1,32 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { type CartItem, type CartState } from '@/utils';
+import { toast } from '@/components/ui/use-toast';
 
-const initialState = {
-  name: 'cart slice',
+const defaultState: CartState = {
+  cartItems: [],
+  numItemsInCart: 0,
+  cartTotal: 0,
+  shipping: 500,
+  tax: 0,
+  orderTotal: 0,
+};
+
+const getCartFromLocalStorage = (): CartState => {
+  const cart = localStorage.getItem('cart');
+  return cart ? JSON.parse(cart) : defaultState;
 };
 
 const cartSlice = createSlice({
   name: 'cart',
-  initialState,
-  reducers: {},
+  initialState: getCartFromLocalStorage(),
+  reducers: {
+    addItem: () => {},
+    clearCart: () => {},
+    removeItem: () => {},
+    editItem: () => {},
+    calculateTotals: () => {},
+  },
 });
 
+export const { addItem, clearCart, removeItem, editItem, calculateTotals } = cartSlice.actions;
 export default cartSlice.reducer;
