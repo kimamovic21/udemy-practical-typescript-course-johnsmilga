797a2a681d9c93f4a43ab5c00a791c9c5cd77bab
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-17 11:25:41 +0200

    182. Toast Component
    -u terminalu instalirana komponenta Shadcn Toaster i generisani fajlovi toast.tsx i toaster.tsx
    -manje izmjene u fajlovima package.json i package-lock.json
    -generisan folder hooks i use-toast.ts fajl
    -u komponentu main.tsx dodana komponenta Toaster

--- 03-store-project/src/main.tsx
+++ 03-store-project/src/main.tsx
@@ -1,11 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './store.ts'
+import { Toaster } from '@/components/ui/toaster'
 import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
+    <Toaster />
     <App />
   </Provider>,
 )
