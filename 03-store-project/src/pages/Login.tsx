fde09dab7f717e408c319e683e639c69668af15a
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-19 15:24:41 +0200

    201. Login Page Setup
    -u komponentu Login.tsx dodane komponente koje sluze kao polja za unos podataka korisnika prilikom prijave

--- 03-store-project/src/pages/Login.tsx
+++ 03-store-project/src/pages/Login.tsx
@@ -1,6 +1,45 @@
+import { Form, Link } from 'react-router-dom'
+import { FormInput, SubmitBtn } from '@/components'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Button } from '@/components/ui/button'
+
 function Login() {
+  const loginAsGuestUser = () => {
+    console.log('guest user')
+  }
+
   return (
-    <div>Login</div>
+    <section className='h-screen grid place-items-center'>
+      <Card className='w-96 bg-muted'>
+        <CardHeader>
+          <CardTitle className='text-center'>Login</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form method='post'>
+            <FormInput type='email' label='email' name='identifier' />
+            <FormInput type='password' name='password' />
+
+            <SubmitBtn text='Login' className='w-full mt-4' />
+
+            <Button
+              type='button'
+              variant='outline'
+              onClick={loginAsGuestUser}
+              className='w-full mt-4'
+            >
+              Guest User
+            </Button>
+
+            <p className='text-center mt-4'>
+              Not a member yet?
+              <Button type='button' asChild variant='link'>
+                <Link to='/register'>Register</Link>
+              </Button>
+            </p>
+          </Form>
+        </CardContent>
+      </Card>
+    </section>
   )
 }
 
