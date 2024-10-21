08ec41fe3404249c3b20a256e9285f24fd18f281
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-21 17:34:15 +0200

    208. Checkout Form Component Setup
    -u types.ts fajlu kreiran i eksportovan tip Checkout
    -u komponenti CheckoutForm.tsx kreirana funkcija action i dodane komponente koje sluze kao polje za unos podataka o dostavi
    -u komponentu App.tsx dodana funkcija action kao checkoutAction

--- 03-store-project/src/App.tsx
+++ 03-store-project/src/App.tsx
@@ -21,70 +21,73 @@ import { loader as checkoutLoader } from './pages/Checkout'
 
 import { action as registerUser } from './pages/Register'
 import { action as loginUser } from './pages/Login'
+import { action as checkoutAction } from './components/CheckoutForm'
+
 import { store } from './store'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+        errorElement: <ErrorElement />,
+        loader: landingLoader
+      },
+      {
+        path: 'products',
+        element: <Products />,
+        errorElement: <ErrorElement />,
+        loader: productsLoader
+      },
+      {
+        path: 'products/:id',
+        element: <SingleProduct />,
+        errorElement: <ErrorElement />,
+        loader: singleProductLoader
+      },
+      {
+        path: 'cart',
+        element: <Cart />,
+        errorElement: <ErrorElement />
+      },
+      { 
+        path: 'about', 
+        element: <About />,
+        errorElement: <ErrorElement />
+      },
+      {
+        path: 'checkout',
+        element: <Checkout />,
+        errorElement: <ErrorElement />,
+        loader: checkoutLoader(store),
+        action: checkoutAction(store),
+      },
+      {
+        path: 'orders',
+        element: <Orders />,
+        errorElement: <ErrorElement />
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+    errorElement: <Error />,
+    action: loginUser(store)
+  },
+  {
+    path: '/register',
+    element: <Register />,
+    errorElement: <Error />,
+    action: registerUser
+  },
+])
+
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <HomeLayout />,
-      errorElement: <Error />,
-      children: [
-        {
-          index: true,
-          element: <Landing />,
-          errorElement: <ErrorElement />,
-          loader: landingLoader
-        },
-        {
-          path: 'products',
-          element: <Products />,
-          errorElement: <ErrorElement />,
-          loader: productsLoader
-        },
-        {
-          path: 'products/:id',
-          element: <SingleProduct />,
-          errorElement: <ErrorElement />,
-          loader: singleProductLoader
-        },
-        {
-          path: 'cart',
-          element: <Cart />,
-          errorElement: <ErrorElement />
-        },
-        { 
-          path: 'about', 
-          element: <About />,
-          errorElement: <ErrorElement />
-        },
-        {
-          path: 'checkout',
-          element: <Checkout />,
-          errorElement: <ErrorElement />,
-          loader: checkoutLoader(store)
-        },
-        {
-          path: 'orders',
-          element: <Orders />,
-          errorElement: <ErrorElement />
-        },
-      ],
-    },
-    {
-      path: '/login',
-      element: <Login />,
-      errorElement: <Error />,
-      action: loginUser(store)
-    },
-    {
-      path: '/register',
-      element: <Register />,
-      errorElement: <Error />,
-      action: registerUser
-    },
-  ])
-  
   return <RouterProvider router={router} />
 }
 
--- 03-store-project/src/components/CheckoutForm.tsx
+++ 03-store-project/src/components/CheckoutForm.tsx
@@ -1,6 +1,20 @@
+import { Form, ActionFunction } from 'react-router-dom'
+import { ReduxStore } from '@/store'
+import FormInput from './FormInput'
+import SubmitBtn from './SubmitBtn'
+
+export const action = (store: ReduxStore): ActionFunction => async ({ request }): Promise<null> => {
+  return null
+}
+
 function CheckoutForm() {
   return (
-    <div>CheckoutForm</div>
+    <Form method='post' className='flex flex-col gap-y-4'>
+      <h4 className='font-medium text-xl mb-4'>Shipping Information</h4>
+      <FormInput label='first name' name='name' type='text' />
+      <FormInput label='address' name='address' type='text' />
+      <SubmitBtn text='Place Your Order' className='mt-4' />
+    </Form>
   )
 }
 
