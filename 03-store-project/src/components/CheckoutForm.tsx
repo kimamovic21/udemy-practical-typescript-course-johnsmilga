4dd7df437c2fbb658cdc205ccf7e5fb1d9d79c01
kimamovic21 <imamovic.kerim@gmail.com>
2024-10-22 14:21:41 +0200

    209. Checkout Form Action
    -u komponentu CheckoutForm.tsx dodana logika za slanje narudzbe

--- 03-store-project/src/components/CheckoutForm.tsx
+++ 03-store-project/src/components/CheckoutForm.tsx
@@ -1,10 +1,58 @@
-import { Form, ActionFunction } from 'react-router-dom'
+import { Form, ActionFunction, redirect } from 'react-router-dom'
 import { ReduxStore } from '@/store'
+import { toast } from '@/hooks/use-toast'
+import { clearCart } from '@/features/cart/cartSlice'
+import { type Checkout, customFetch, formatAsDollars } from '@/utils'
 import FormInput from './FormInput'
 import SubmitBtn from './SubmitBtn'
 
-export const action = (store: ReduxStore): ActionFunction => async ({ request }): Promise<null> => {
-  return null
+export const action = (store: ReduxStore): ActionFunction => async ({ request }): Promise<null | Response> => {
+  const formData = await request.formData()
+  const name = formData.get('name') as string
+  const address = formData.get('name') as string
+
+  if (!name || !address) {
+    toast({ description: 'please fill out all fields' })
+    return null
+  }
+
+  const user = store.getState().userState.user
+  if (!user) {
+    toast({ description: 'please login to place an order' })
+    return redirect('/login')
+  }
+
+  const { cartItems, orderTotal, numItemsInCart } = store.getState().cartState
+
+  const info: Checkout = {
+    name,
+    address,
+    chargeTotal: orderTotal,
+    orderTotal: formatAsDollars(orderTotal),
+    cartItems,
+    numItemsInCart,
+  }
+
+  try {
+    await customFetch.post('/orders',
+      { 
+        data: info 
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${user.jwt}`,
+        },
+      }
+    )
+
+    store.dispatch(clearCart())
+    toast({ description: 'order placed' })
+    return redirect('/orders')
+  } catch (error) {
+    toast({ description: 'order failed' })
+    console.error(error) 
+    return null
+  }
 }
 
 function CheckoutForm() {
